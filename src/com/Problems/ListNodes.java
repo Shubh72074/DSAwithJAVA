d2fc84e9b34e2a8bc5c191b960dd7aad01970d8f
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-08-18 23:52:12 +0530

    DAY 8 of Learning DSA with JAVA

--- src/com/Problems/ListNodes.java
+++ src/com/Problems/ListNodes.java
@@ -3,29 +3,41 @@ package com.Problems;
 public class ListNodes {
     int val;
     ListNodes next;
-    ListNodes() {};
+    ListNodes() {}
     ListNodes(int val) {this.val = val;}
     ListNodes(int val, ListNodes next) {this.val = val; this.next = next;}
 
     public static void main(String[] args) {
         ListNodes shead = new ListNodes();
         ListNodes head = shead;
+        int[] arr = {1,1,1,2,3,3,3};
         for (int i = 1; i < 7; i++) {
-            head.val = i;
+            head.val = arr[i];
             head.next = new ListNodes();
             head = head.next;
         }
         head.next = null;
-        RemoveDuplicateNodes.deleteDuplicate(shead);
+        RemoveDuplicateNodes.deleteDuplicates(shead);
+        while (shead != null) {
+            System.out.println(shead.val);
+            shead = shead.next;
+        }
     }
 }
 
 class RemoveDuplicateNodes {
-    static void deleteDuplicate(ListNodes head) {
+    static void deleteDuplicates(ListNodes head) {
+        if (head == null) return;
         ListNodes temp = head;
-        while (temp != null) {
-            System.out.println(temp.val);
+        while (temp.next != null) {
+            int cur = temp.val;
+            int nxt = temp.next.val;
+            if (cur == nxt ) {
+                temp.next = temp.next.next;
+                continue;
+            }
             temp = temp.next;
         }
+
     }
 }
