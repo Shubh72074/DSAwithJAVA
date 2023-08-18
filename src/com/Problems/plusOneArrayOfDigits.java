05c6b9393fe2591dba740c74eb31502d3ec2864a
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-08-18 23:52:48 +0530

    DAY 8 of Learning DSA with JAVA

---
+++ src/com/Problems/plusOneArrayOfDigits.java
@@ -0,0 +1,14 @@
+package com.Problems;
+
+import java.util.Arrays;
+
+public class plusOneArrayOfDigits {
+    public static void main(String[] args) {
+        int[] digits = {9,9,9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+    static int[] plusOne(int[] digits) {
+       int[] arr = {0};
+       return  arr;
+    }
+}
