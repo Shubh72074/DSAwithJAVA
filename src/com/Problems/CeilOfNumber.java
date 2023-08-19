1f570f56e938b98656cf9630a7987d61211ac48c
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-08-19 17:44:59 +0530

    DAY 9 of Learning DSA with JAVA

---
+++ src/com/Problems/CeilOfNumber.java
@@ -0,0 +1,28 @@
+package com.Problems;
+
+public class CeilOfNumber {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5,7,9,12,17,23,28,32};
+        int target = 6;
+
+        System.out.println(ceilOf(arr,target));
+    }
+
+    static int ceilOf(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end ){
+            int mid = start + (end - start ) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (arr[mid] > target) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return arr[start % arr.length];
+    }
+}
