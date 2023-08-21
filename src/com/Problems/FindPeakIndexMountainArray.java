f518941a2d6ee70b568e1c26ea2dab5c911e3703
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-08-21 19:59:26 +0530

    DAY 11 of Learning DSA with JAVA

--- src/com/Problems/FindPeakIndexMountainArray.java
+++ src/com/Problems/FindPeakIndexMountainArray.java
@@ -2,8 +2,16 @@ package com.Problems;
 
 public class FindPeakIndexMountainArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,4,5,8,3,2,1,0};
-        System.out.println(returnPeak(arr));
+        int[] arr = {1,2,400,51,8,3,2,1,0};
+        int target = 1;
+        int peak = returnPeak(arr);
+        int search = findIndex(arr, target, 0, peak);
+
+        if ( search == -1) {
+            search = findIndex(arr, target, peak + 1, arr.length - 1);
+        }
+
+        System.out.println(search);
     }
     static int returnPeak(int[] arr) {
         int start = 0;
@@ -18,6 +26,37 @@ public class FindPeakIndexMountainArray {
             }
         }
         return end;
+    }
+    static int findIndex(int[] arr, int target, int start, int end) {
+        if (arr[start] < arr[end]) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (target == arr[mid]) {
+                    return mid;
+                }
+                else if (arr[mid] > target) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+        }
+        else {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (target == arr[mid]) {
+                    return mid;
+                }
+                else if (arr[mid] > target) {
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 }
 
