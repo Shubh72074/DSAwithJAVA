fe4eed863fead7f45c7f37ba35ffb203c6d70b7e
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-08-23 11:20:49 +0530

    DAY 13 of Learning DSA with JAVA

---
+++ src/com/Problems/StringToInteger.java
@@ -0,0 +1,26 @@
+package com.Problems;
+
+public class StringToInteger {
+    public static void main(String[] args) {
+        String nn = "42";
+        long ans = 0;
+        boolean neg = false;
+        String[] n;
+        n = nn.trim().split(" ");
+        for (char ch : n[0].toCharArray()) {
+            if (ch == '-') {
+                neg = true;
+            }
+            if((int)ch >= 48 && (int)ch <= 57) {
+                int tm = Integer.parseInt(String.valueOf(ch));
+                ans = ans * 10 + tm;
+            }
+        }
+        if (neg) {
+            System.out.println(ans * -1);
+        }
+        else {
+            System.out.println(ans);
+        }
+    }
+}
--- src/com/Problems/integerMax.java
+++ src/com/Problems/integerMax.java
@@ -2,7 +2,7 @@ package com.Problems;
 
 public class integerMax {
     public static void main(String[] args) {
-        int a = 1534236469;
+        int a = 42;
         long ans = 0;
         while (a != 0) {
             int rem = a % 10;
