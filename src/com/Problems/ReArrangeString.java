a97bb31d7105d479ca8ee56cb6c701bc1aa0adc2
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-08-24 23:10:18 +0530

    DAY 14 of Learning DSA with JAVA

--- src/com/Problems/ReArrangeString.java
+++ src/com/Problems/ReArrangeString.java
@@ -1,40 +1,23 @@
 package com.Problems;
 
+import java.util.HashMap;
+
 public class ReArrangeString {
     public static void main(String[] args) {
         String s = "aba";
         System.out.println(reArrange(s));
     }
     static String reArrange(String s) {
-        if (s.length() == 1) return s;
-        char[] arr = s.toCharArray();
-        int cur = 0;
-        int next = 1;
-        int len = s.length();
-        while (next < len) {
-            if (arr[cur] == arr[next]) {
-                if (!swapped(next,len,next,arr)) return "";
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (char ch: s.toCharArray()) {
+            if (charCount.containsKey(ch)) {
+                charCount.put(ch,charCount.get(ch) + 1);
             }
-            cur++;
-            next++;
-        }
-        StringBuilder ans = new StringBuilder();
-        for (char ch: arr) ans.append(ch);
-        return ans.toString();
-    }
-    static boolean swapped(int temp, int len, int next, char[] arr) {
-        while (temp < len) {
-            if (arr[next] != arr[temp]) {
-                return swap(arr,next,temp);
+            else {
+                charCount.put(ch,1);
             }
-            temp++;
         }
-        return false;
-    }
-    static boolean swap(char[] arr, int a, int b) {
-        char temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
-        return true;
+        System.out.println(charCount);
+        return "ALOO";
     }
 }
