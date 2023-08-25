c26985c6750eb4c773f6e0d821a60f21a6477571
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-08-25 23:58:54 +0530

    DAY 15 of Learning DSA with JAVA

---
+++ src/com/Algorithms/StackList.java
@@ -0,0 +1,29 @@
+package com.Algorithms;
+
+import java.util.Vector;
+
+public class StackList {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        int[] nums = {1,2,3,4,5,6};
+        for (int num: nums) {
+            System.out.println(stack.push(num));
+        }
+        System.out.println("Printing All elements:");
+        stack.printItems();
+    }
+}
+class Stack {
+    Vector<Integer> arr = new Vector<>();
+    protected String push(int data) {
+        arr.add(data);
+        return "ADDED ITEM";
+    }
+
+    protected void printItems() {
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            System.out.println(arr.get(i));
+        }
+    }
+
+}
