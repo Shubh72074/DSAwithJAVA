3279fc26ed3d7e9839075c6bef3b2e4897e420d9
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-01 11:43:12 +0530

    Bubble Sorting..

---
+++ src/com/Algorithms/BubbleSortAlgo.java
@@ -0,0 +1,30 @@
+package com.Algorithms;
+
+import java.util.Arrays;
+
+public class BubbleSortAlgo {
+    public static void main(String[] args) {
+        int[] arr = {4,32,1,9,12,67,12,11,5};
+        System.out.println(Arrays.toString(sortedArray(arr)));
+    }
+    static int[] sortedArray(int[] arr) {
+        for (int i=0; i<arr.length; i++) {
+            boolean swapped = false;
+            for (int j=1; j<arr.length-i; j++) {
+                if (arr[j] < arr[j-1]) {
+                    swapped = swap(arr,j,j-1);
+                }
+            }
+            if (!swapped) {
+                return arr;
+            }
+        }
+        return arr;
+    }
+    static boolean swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        return true;
+    }
+}
