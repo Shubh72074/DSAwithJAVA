94a15a32e69289eaa4645ed59c68af0bee36794f
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-02 22:00:29 +0530

    Cycle Sorting..

---
+++ src/com/Algorithms/CycleSortAlgo.java
@@ -0,0 +1,26 @@
+package com.Algorithms;
+
+import java.util.Arrays;
+
+import static com.Algorithms.BubbleSortAlgo.swap;
+
+public class CycleSortAlgo {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        cycleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cycleSort(int[] arr) {
+        int i = 0;
+        while(i<arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr,i,arr[i]-1);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+}
