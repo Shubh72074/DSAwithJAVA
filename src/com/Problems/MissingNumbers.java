2612d38b9c354a0263c45e0285a85f50c434e8ac
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-03 15:32:27 +0530

    Missing Number Problem..

--- src/com/Problems/MissingNumbers.java
+++ src/com/Problems/MissingNumbers.java
@@ -1,32 +1,32 @@
 package com.Problems;
 
+import java.util.Arrays;
+
 public class MissingNumbers {
     public static void main(String[] args) {
-        int[] arr = {1,3,5,2,4};
-        int n = 5;
+        int[] arr = {1,3,4,2,5,5};
 
-        System.out.println(findMissingNumber(arr,n));
+        System.out.println(findMissingNumber(arr));
     }
 
-    static int findMissingNumber(int[] arr, int n) {
+    static int findMissingNumber(int[] arr) {
         int i=0;
         while(i<arr.length) {
-            if (arr[i] == n) {
-                i++;
-            }
-            else if (arr[i] != i) {
-                swap(arr,i,arr[i]);
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr,i,arr[i]-1);
             }
             else {
                 i++;
             }
         }
+        System.out.println(Arrays.toString(arr));
         for (int j=0; j<arr.length; j++) {
-            if (arr[j] != j) {
-                return j;
+            if (arr[j] != j+1) {
+                return j+1;
             }
         }
-        return n;
+        return -1;
     }
 
     static void swap(int[] arr, int first, int second) {
