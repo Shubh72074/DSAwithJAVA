f01d1ca3eb2786c2fcb292ad39d4f9ccc1b2f5f7
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-04 20:08:28 +0530

    updated

--- src/com/Problems/MissingNumbers.java
+++ src/com/Problems/MissingNumbers.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MissingNumbers {
     public static void main(String[] args) {
-        int[] arr = {1,3,4,2,5,5};
+        int[] arr = {1,3,1};
 
         System.out.println(findMissingNumber(arr));
     }
