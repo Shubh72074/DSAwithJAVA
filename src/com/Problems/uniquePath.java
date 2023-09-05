d2ba247438f0af880af9ef9ad23cb5ae070a79c2
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-05 23:24:02 +0530

    uniquePath

---
+++ src/com/Problems/uniquePath.java
@@ -0,0 +1,17 @@
+package com.Problems;
+
+import java.util.Arrays;
+
+public class uniquePath {
+    public static void main(String[] args) {
+        int m=3;
+        int n=6;
+        System.out.println(findUniquePaths(m,n));
+    }
+
+    static int findUniquePaths(int m, int n) {
+        int[][] grid = new int[m][n];
+        
+        return 0;
+    }
+}
