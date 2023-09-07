37cda50d1dc8d9629145ec0ff592e2cc72759eb2
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-07 23:12:56 +0530

    Stack

--- src/com/Random/ListInterface.java
+++ src/com/Random/ListInterface.java
@@ -2,13 +2,22 @@ package com.Random;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class ListInterface {
     public static void main(String[] args) {
-        List list = new ArrayList();
-        list.add(0);
-        list.add(1);
-        System.out.println(list);
-        System.out.println(list.contains(1));
+//        List list = new ArrayList();
+//        list.add(0);
+//        list.add(1);
+//        System.out.println(list);
+//        System.out.println(list.contains(1));
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        System.out.println(stack.pop());
+
     }
 }
