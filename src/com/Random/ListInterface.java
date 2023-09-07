5c1eeaaade9df677dbc7b7d6efec0e41681132df
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-07 23:16:01 +0530

    Queue

--- src/com/Random/ListInterface.java
+++ src/com/Random/ListInterface.java
@@ -1,8 +1,6 @@
 package com.Random;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class ListInterface {
     public static void main(String[] args) {
@@ -12,12 +10,20 @@ public class ListInterface {
 //        System.out.println(list);
 //        System.out.println(list.contains(1));
 
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(1);
+//        stack.push(2);
+//        stack.push(3);
+//
+//        System.out.println(stack.pop());
 
-        System.out.println(stack.pop());
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println(q.remove());
 
     }
 }
