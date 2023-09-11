51bcc9873dd749d24dff766ffec8aba89bdb504f
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-11 23:34:55 +0530

    Remove a Character within a String

---
+++ src/com/Problems/RemoveChar.java
@@ -0,0 +1,15 @@
+package com.Problems;
+
+public class RemoveChar {
+    public static void main(String[] args) {
+        removeCharFromString("abcbcdjfkd", 'c');
+        removeCharFromString("ABCCCCC", 'C');
+        removeCharFromString("shubha", 'a');
+
+    }
+
+    private static void removeCharFromString(String input, char c) {
+        String result = input.replaceAll(String.valueOf(c), "");
+        System.out.println(result);
+    }
+}
