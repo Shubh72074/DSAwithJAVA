9142db2ee3b1aae1cb6d6e97bb10fc1b75b7da5f
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-14 22:25:09 +0530

    modification

--- src/com/Problems/FindAllMissingNumbers.java
+++ src/com/Problems/FindAllMissingNumbers.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class FindAllMissingNumbers {
     public static void main(String[] args) {
-        int[] arr = {1,3,4,2,5,5,5,5};
+        int[] arr = {1,3,4,2,5,5,5,5,6,7,8,8};
 
         System.out.println(findMissingNumber(arr));
     }
