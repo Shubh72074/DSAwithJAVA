3efea9d8be6f52ddab747bf332ec420e708c2677
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-16 23:05:14 +0530

    modification

--- src/com/Problems/CeilOfNumber.java
+++ src/com/Problems/CeilOfNumber.java
@@ -4,7 +4,6 @@ public class CeilOfNumber {
     public static void main(String[] args) {
         int[] arr = {1,2,3,5,7,9,12,17,23,28,32,35,39,56};
         int target = 36;
-
         System.out.println(ceilOf(arr,target));
     }
 
