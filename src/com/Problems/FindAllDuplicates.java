a7c471ced82088049b951f2dbd0b12e3a9d67339
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-16 23:07:56 +0530

    modification

--- src/com/Problems/FindAllDuplicates.java
+++ src/com/Problems/FindAllDuplicates.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class FindAllDuplicates {
     public static void main(String[] args) {
-        int[] arr = {4,3,2,7,8,2,3,1};
+        int[] arr = {4,3,2,7,4,8,2,3,1};
 
         System.out.println(findDuplicate(arr));
     }
