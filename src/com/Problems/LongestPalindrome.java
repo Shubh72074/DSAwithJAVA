4622c6620c3bed96d8891a1d9e8f26271ab5ab3f
Shubh72074 <Shubhamnishadr16@gmail.com>
2023-09-19 23:52:18 +0530

    modification

--- src/com/Problems/LongestPalindrome.java
+++ src/com/Problems/LongestPalindrome.java
@@ -2,7 +2,7 @@ package com.Problems;
 
 public class LongestPalindrome {
     public static void main(String[] args) {
-        String s = "abccds";
+        String s = "abcyyttffcds";
         System.out.println(findAns(s));
     }
     static String findAns(String s) {
